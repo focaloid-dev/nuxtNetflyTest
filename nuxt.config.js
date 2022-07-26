85bdb89261b8b43ac62a147c255e80734361224d
Musthak <musthak@focaloid.com>
2022-07-26 16:50:51 +0530

    Nuxt test code

--- nuxt.config.js
+++ nuxt.config.js
@@ -36,9 +36,13 @@ export default {
 
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
+    '@nuxt/content'
   ],
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
+  },
+  content: {
+    liveEdit: false
   }
 }
