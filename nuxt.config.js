59758922d7bd0a62921f1c8e6579ad0d59f3d4c9
alshoja-focaloid <74759660+focaloid-dev@users.noreply.github.com>
2023-06-26 14:27:03 +0530

    fix: test

--- nuxt.config.js
+++ nuxt.config.js
@@ -29,7 +29,7 @@ export default {
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
-  target: 'static',
+  target: 'server',
 
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
